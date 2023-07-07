994b368fec7c856ac6182c4659bea6ca7fd9bafd
Anshul Patel <anshulpatel5757@gmail.com>
2023-07-07 21:27:32 +0530

    fixed scaleImg-err and Properly working in LocalHost till now

--- src/util.js
+++ src/util.js
@@ -1,10 +1,15 @@
-
 const scaleImg = (ImagePath, size) => {
-    const image = ImagePath.match(/media\/screenshots/)
-        ? ImagePath.replace("media/screenshots", `media/resize/${size}/-/screenshots`)
-        : ImagePath.replace("media/games", `media/resize/${size}/-/games`);
+  if (ImagePath === null) {
+    return "https://cdn.pixabay.com/photo/2020/04/22/18/22/excuse-me-5079442_1280.jpg";
+  }
+  const image = ImagePath.match(/media\/screenshots/)
+    ? ImagePath.replace(
+        "media/screenshots",
+        `media/resize/${size}/-/screenshots`
+      )
+    : ImagePath.replace("media/games", `media/resize/${size}/-/games`);
 
-    return image;
+  return image;
 };
 
 export default scaleImg;
